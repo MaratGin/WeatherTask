404ef339e00eefb5914a75039de5d4bb038e2ca4
Marat <gG3-Kg4-V0E-aAX>
2021-03-27 18:58:00 +0300

    Adaptation for windows

--- src/com/company/СSVTask/FileWriter.java
+++ src/com/company/СSVTask/FileWriter.java
@@ -16,8 +16,8 @@ public class FileWriter {
 
         File file= new File(path);
 
-        //File file1= new File(file.getParent()+"\\Result.txt"); Винда
-        File file1= new File(file.getParent()+"/Result.txt");
+        File file1= new File(file.getParent()+"\\Result.txt"); //Винда
+       // File file1= new File(file.getParent()+"/Result.txt"); MacOS
 
         if (!file1.exists()){
             file1.createNewFile();
